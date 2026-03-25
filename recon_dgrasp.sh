482efcb87fbb24fe1d26cbb81dc37ea852888b51
Martin Kostal <martinkostalmk@gmail.com>
2026-03-25 15:05:34 -0400

    udate recon_drgasp

--- recon_dgrasp.sh
+++ recon_dgrasp.sh
@@ -1,20 +1,24 @@
 #/bin/bin -l
 
-#for f in $(seq -w 136 300); do
-    f="10"
-    #DIR="/home/genekim/projects/dgrasp_recon/gsi_arm1/1104"
-    DIR="~/data"
-    DATA="slice${f}" # without .h5
+for f in $(seq 1 120); do
+    
 
-    SPOKES=13
-
+    DIR="~/data/"
+    DATA="slice${f}" # without .h5    
+    SPOKES=8
+    
+    f_padded=$(printf "%03d" $f)  # zero-pad the number to 3 digits
+    COIL_MAP="coil_map_slice_${f_padded}" 
+  
     echo "> DIR: ${DIR}"
     echo "> DATA: ${DATA}"
+    echo "> COIL_MAP: ${COIL_MAP}"
     echo "> SPOKES: ${SPOKES}"
 
     # reconstruct slice by slice
-    python dce_recon_dgrasp_echoes.py --dir ${DIR} --data ${DATA} --spokes_per_frame ${SPOKES} 
+    python dce_recon_dgrasp_echoes.py --dir ${DIR} --data ${DATA} --coil_map ${COIL_MAP} --spokes_per_frame ${SPOKES} 
+
 
     # convert the .h5 file to dicom
     # python dcm_recon.py --dir ${DIR} --h5py ${DATA} --spokes_per_frame ${SPOKES}
-#done
+done
